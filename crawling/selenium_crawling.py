aace566947273345cec1555fc1d03f4d91a0bbb7
leedokchidok19 <duckhyun4433@naver.com>
2020-07-20 12:38:33 +0000

    selenium crawling

---
+++ crawling/selenium_crawling.py
@@ -0,0 +1,16 @@
+import selenium    # pip install selenium
+from selenium import webdriver
+
+search_term = '박보영'
+url = f'https://www.google.co.in/search?q={search_term}&tbm=isch'
+
+browser = webdriver.Chrome('chromedriver.exe')
+browser.get(url)
+
+for i in range(200):
+    browser.execute_script('window.scrollBy(0,10000)')
+
+for idx, el in enumerate(browser.find_elements_by_class_name('rg_ic')):
+    el.screenshot(str(idx) + '.png')
+
+browser.close()
